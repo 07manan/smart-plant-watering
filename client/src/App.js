a60e257696bcdebd1e818056ac3b4f531e6d8a50
07manan <07manan1850@gmail.com>
2023-11-22 21:20:31 +0530

    env url

--- client/src/App.js
+++ client/src/App.js
@@ -22,37 +22,35 @@ function App() {
     ],
   });
 
-  const getData = () => {
-    axios
-      .get(process.env.REACT_BACKEND_URL + "/get-moisture-data")
-      .then((res) => {
-        let labels = [];
-        let data = [];
-        res.data.forEach((element) => {
-          labels.push(element.timeStamp);
-          data.push(element.moisture);
-        });
-        setData({
-          labels: labels,
-          datasets: [
-            {
-              label: "moisture",
-              data: data,
-              backgroundColor: [
-                "rgba(75,192,192,1)",
-                "#ecf0f1",
-                "#50AF95",
-                "#f3ba2f",
-                "#2a71d0",
-              ],
-            },
-          ],
-        });
+  const getData = async () => {
+    const url = process.env.REACT_APP_BACKEND_URL + "/get-moisture-data";
+    await axios.get(url).then((res) => {
+      let labels = [];
+      let data = [];
+      res.data.forEach((element) => {
+        labels.push(element.timeStamp);
+        data.push(element.moisture);
       });
+      setData({
+        labels: labels,
+        datasets: [
+          {
+            label: "moisture",
+            data: data,
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0",
+            ],
+          },
+        ],
+      });
+    });
   };
 
   useEffect(() => {
-    getData();
     let interval = setInterval(() => {
       getData();
     }, 1000);
